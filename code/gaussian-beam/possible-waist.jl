c894941508fb8da0ae16b42fae108c2a0926a23d
Hugo Trentesaux <hugo@trentesaux.fr>
2020-09-07 14:59:59 +0200

    change possible waist parameters

--- code/gaussian-beam/possible-waist.jl
+++ code/gaussian-beam/possible-waist.jl
@@ -11,9 +11,9 @@ n = 1       # medium refraction index
 # different possible beam profiles
 
 N = 13 # number of beams to plot
-z = range(-350e-6, 350e-6; length=1000)     # z range
-wa = range(4.2e-6,5.7e-6, length=N)'        # range of waists for 1P
-wb = range(5.8e-6,7.3e-6, length=N)'        # range of waists for 2P
+z = range(-200e-6, 200e-6; length=1000)     # z range
+wa = range(2.2e-6,3.7e-6, length=N)'        # range of waists for 1P
+wb = range(3.8e-6,5.3e-6, length=N)'        # range of waists for 2P
 lw = ones(N)'; lw[Int(ceil(N/2))] = 2;      # line widths
 
 # plot it 
@@ -23,6 +23,7 @@ plot!(z*1e6, gaussian_width.(n,z,wb,λ2)*1e6, color=RGBA(1,0,0,0.7), lw=lw)
 title!("optimal waist for light sheet in water")
 xlabel!("distance to waist (µm)")
 ylabel!("beam width (µm)")
+ylims!(0,15);
 
 gui()
 
